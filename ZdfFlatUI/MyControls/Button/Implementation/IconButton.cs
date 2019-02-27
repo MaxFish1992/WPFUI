6796cb11f5eb750b6facfd32feb0da50a69d5ec3
MaxFish <348756718@qq.com>
2019-02-27 15:25:46 +0800

    添加项目文件。

---
+++ ZdfFlatUI/MyControls/Button/Implementation/IconButton.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ZdfFlatUI
+{
+    /// <summary>
+    /// 按照步骤 1a 或 1b 操作，然后执行步骤 2 以在 XAML 文件中使用此自定义控件。
+    ///
+    /// 步骤 1a) 在当前项目中存在的 XAML 文件中使用该自定义控件。
+    /// 将此 XmlNamespace 特性添加到要使用该特性的标记文件的根 
+    /// 元素中: 
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:ZdfFlatUI.MyControls.Button.Implementation"
+    ///
+    ///
+    /// 步骤 1b) 在其他项目中存在的 XAML 文件中使用该自定义控件。
+    /// 将此 XmlNamespace 特性添加到要使用该特性的标记文件的根 
+    /// 元素中: 
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:ZdfFlatUI.MyControls.Button.Implementation;assembly=ZdfFlatUI.MyControls.Button.Implementation"
+    ///
+    /// 您还需要添加一个从 XAML 文件所在的项目到此项目的项目引用，
+    /// 并重新生成以避免编译错误: 
+    ///
+    ///     在解决方案资源管理器中右击目标项目，然后依次单击
+    ///     “添加引用”->“项目”->[浏览查找并选择此项目]
+    ///
+    ///
+    /// 步骤 2)
+    /// 继续操作并在 XAML 文件中使用控件。
+    ///
+    ///     <MyNamespace:IconButton/>
+    ///
+    /// </summary>
+    public class IconButton : Button
+    {
+        #region 图标相关属性
+
+        public double ImageSize
+        {
+            get { return (double)GetValue(ImageSizeProperty); }
+            set { SetValue(ImageSizeProperty, value); }
+        }
+        public static readonly DependencyProperty ImageSizeProperty = DependencyProperty.Register("ImageSize", typeof(double), typeof(IconButton),
+            new FrameworkPropertyMetadata(30.0, FrameworkPropertyMetadataOptions.AffectsRender));
+
+
+        public string ImagePath
+        {
+            get { return (string)GetValue(ImagePathProperty); }
+            set { SetValue(ImagePathProperty, value); }
+        }
+        public static readonly DependencyProperty ImagePathProperty = DependencyProperty.Register("ImagePath", typeof(string), typeof(IconButton),
+                new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.AffectsRender, ImageSourceChanged));
+
+        #endregion
+
+        #region 按钮相关属性
+
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register("CornerRadius"
+            , typeof(CornerRadius), typeof(IconButton));
+        /// <summary>
+        /// 按钮四周圆角
+        /// </summary>
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        public static readonly DependencyProperty MouseOverBackgroundProperty = DependencyProperty.Register("MouseOverBackground"
+            , typeof(Brush), typeof(IconButton));
+        /// <summary>
+        /// 鼠标悬浮时按钮的背景色
+        /// </summary>
+        public Brush MouseOverBackground
+        {
+            get { return (Brush)GetValue(MouseOverBackgroundProperty); }
+            set { SetValue(MouseOverBackgroundProperty, value); }
+        }
+
+        public static readonly DependencyProperty PressedBackgroundProperty = DependencyProperty.Register("PressedBackground"
+            , typeof(Brush), typeof(IconButton));
+        /// <summary>
+        /// 鼠标按下时按钮的背景色
+        /// </summary>
+        public Brush PressedBackground
+        {
+            get { return (Brush)GetValue(PressedBackgroundProperty); }
+            set { SetValue(PressedBackgroundProperty, value); }
+        }
+
+        #endregion
+        static IconButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(IconButton), new FrameworkPropertyMetadata(typeof(IconButton)));
+        }
+
+        #region 事件
+        //依赖属性发生改变时候触发
+        private static void ImageSourceChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+
+            try
+            {
+                Application.GetResourceStream(new Uri("pack://application:,,," + (string)e.NewValue));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + ex.StackTrace);
+            }
+
+        }
+        #endregion 事件
+    }
+}
